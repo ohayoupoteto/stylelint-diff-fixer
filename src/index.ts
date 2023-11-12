f696a063a488e20c467d9465d6db94f11aafd496
oteto <94293017+ohayoupoteto@users.noreply.github.com>
2023-11-12 23:20:56 +0900

    feat: CLI対応

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
+#!/usr/bin/env node
 import { StyleLinter } from './style-linter';
 import { StyleFormatter } from './style-formatter';
 import { Commander } from './commander';
 import { DiffParser } from './diff-parser';
 import { CssFileHandler } from './css-file-handler';
 
-export async function run(): Promise<void> {
-  const diffParser = new DiffParser(new Commander());
-  const diffsEachFile = diffParser.getDiffsEachFile();
-  if (diffsEachFile.length === 0) return;
-
+const diffParser = new DiffParser(new Commander());
+const diffsEachFile = diffParser.getDiffsEachFile();
+if (diffsEachFile.length !== 0) {
   const styleLinter = new StyleLinter(new StyleFormatter());
   const cssFileHandler = new CssFileHandler(styleLinter);
-  await cssFileHandler.update(diffsEachFile);
+  cssFileHandler.update(diffsEachFile);
 }
