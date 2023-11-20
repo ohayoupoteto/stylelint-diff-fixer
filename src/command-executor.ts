c5112592be7cc3615fabc49653faf3fe744c4bd8
oteto <94293017+ohayoupoteto@users.noreply.github.com>
2023-11-20 23:39:06 +0900

    fix: CommandExecutorにエラーハンドリングを追加

--- src/command-executor.ts
+++ src/command-executor.ts
@@ -2,25 +2,36 @@ import { execSync } from 'child_process';
 
 export class CommandExecutor {
   gitAddUncommittedFile(): void {
-    execSync('git add --intent-to-add .');
+    this.exec('git add --intent-to-add .');
   }
 
   gitDiff(filepath: string): string {
-    return execSync(
+    return this.exec(
       `git --no-pager diff HEAD -U0 "*.css" "*.scss" ${filepath}`,
-    ).toString();
+    );
   }
 
   gitDiffNameonly(filepath: string): string {
-    return execSync(
-      `git diff --name-only "*.css" "*.scss" ${filepath}`,
-    ).toString();
+    return this.exec(`git diff --name-only "*.css" "*.scss" ${filepath}`);
   }
 
   /**
    * Gitのルートへのフルパスを取得
    */
   gitRootPath(): string {
-    return execSync('git rev-parse --show-toplevel').toString().trimEnd();
+    return this.exec('git rev-parse --show-toplevel').trimEnd();
+  }
+
+  private exec(command: string): string {
+    try {
+      return execSync(command).toString();
+    } catch (e: unknown) {
+      if (this.hasStderr(e)) process.exit(1);
+      throw e;
+    }
+  }
+
+  private hasStderr(err: any): err is { stderr: Buffer } {
+    return !!err.stderr;
   }
 }
