e74773150244394045cdcddfb325e461db3562c3
oteto <94293017+ohayoupoteto@users.noreply.github.com>
2023-11-21 21:42:08 +0900

    feat: main.tsに一部処理を切り出す

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,10 @@
 #!/usr/bin/env node
-import { StyleFixer } from './style/style-fixer';
-import { StyleAdjuster } from './style/style-adjuster';
-import { CommandExecutor } from './command-executor';
-import { DiffParser } from './diff/diff-parser';
-import { CssFileHandler } from './css-file/css-file-handler';
-import { Logger } from './log/logger';
-import { LogStyle } from './log/log-style';
-import { CssFileSystem } from './css-file/css-file-system';
 import { Cli } from './cli';
 import { Command } from 'commander';
+import { main } from './main';
 
 const cli = new Cli(new Command());
 cli.init();
 const { filepath } = cli.getOptions();
 
-const commandExecutor = new CommandExecutor();
-const diffParser = new DiffParser(commandExecutor);
-
-const diffsEachFile = diffParser.getDiffsEachFile(filepath);
-if (diffsEachFile.length !== 0) {
-  const styleFixer = new StyleFixer(new StyleAdjuster());
-  const cssFileSystem = new CssFileSystem();
-  const logger = new Logger(new LogStyle());
-
-  const cssFileHandler = new CssFileHandler(
-    styleFixer,
-    cssFileSystem,
-    logger,
-    commandExecutor.gitRootPath(),
-  );
-  cssFileHandler.update(diffsEachFile);
-}
+main(filepath);
---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import { CommandExecutor } from './command-executor';
+import { DiffParser } from './diff/diff-parser';
+import { StyleFixer } from './style/style-fixer';
+import { StyleAdjuster } from './style/style-adjuster';
+import { CssFileSystem } from './css-file/css-file-system';
+import { Logger } from './log/logger';
+import { LogStyle } from './log/log-style';
+import { CssFileHandler } from './css-file/css-file-handler';
+
+export async function main(filepath: string): Promise<void> {
+  const commandExecutor = new CommandExecutor();
+  const diffParser = new DiffParser(commandExecutor);
+
+  const diffsEachFile = diffParser.getDiffsEachFile(filepath);
+  if (diffsEachFile.length !== 0) {
+    const styleFixer = new StyleFixer(new StyleAdjuster());
+    const cssFileSystem = new CssFileSystem();
+    const logger = new Logger(new LogStyle());
+
+    const cssFileHandler = new CssFileHandler(
+      styleFixer,
+      cssFileSystem,
+      logger,
+      commandExecutor.gitRootPath(),
+    );
+    await cssFileHandler.update(diffsEachFile);
+  }
+}
