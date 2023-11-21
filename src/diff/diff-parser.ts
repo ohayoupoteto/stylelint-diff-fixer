a9b73dfb24306a54ecd93f60a2c49d6e2881eaab
oteto <94293017+ohayoupoteto@users.noreply.github.com>
2023-11-21 23:32:52 +0900

    fix: 加行が無いファイルはfixをスキップする

--- src/diff/diff-parser.ts
+++ src/diff/diff-parser.ts
@@ -1,5 +1,6 @@
 import type { CommandExecutor } from '../command-executor';
 import type { DiffEachFile, DiffHunk } from './diff.type';
+import { isEmptyArray } from '../utils/array';
 
 const HUNK_HEADER_PATTERN = /@@\s.*\+(\d+)(,(\d+))?\s@@/;
 
@@ -17,35 +18,50 @@ export class DiffParser {
       .gitDiff(filepath)
       .split(/\ndiff --git .+?\n/);
 
-    return diffFileStrs.map((diff, i) => {
-      const diffHunkStrs = diff.split(/\n(?=@@\s.*\s@@.*?\n)/);
-      diffHunkStrs.shift(); // headerの削除
+    return diffFileStrs
+      .map((diff, i) => {
+        const diffHunkStrs = diff.split(/\n(?=@@\s.*\s@@.*?\n)/);
+        diffHunkStrs.shift(); // headerの削除
 
-      return {
-        hunks: this.createDiffHunks(diffHunkStrs),
-        filepath: filepaths[i],
-      };
-    });
+        const hunks = this.createDiffHunks(diffHunkStrs);
+        if (isEmptyArray(hunks)) return;
+
+        return {
+          hunks,
+          filepath: filepaths[i],
+        };
+      })
+      .filter(
+        (
+          diffEachFile,
+        ): diffEachFile is Exclude<typeof diffEachFile, undefined> =>
+          diffEachFile !== undefined,
+      );
   }
 
   private createDiffHunks(diffHunkStrs: string[]): DiffHunk[] {
-    return diffHunkStrs.map((diffHunkStr) => {
-      const matches = diffHunkStr.match(HUNK_HEADER_PATTERN);
-      if (!matches) throw new Error();
-      const [, startLineNumber, , count] = matches;
+    return diffHunkStrs
+      .map((diffHunkStr) => {
+        const matches = diffHunkStr.match(HUNK_HEADER_PATTERN);
+        if (!matches) throw new Error();
+        const [, startLineNumber, , count] = matches;
 
-      const addedRows = diffHunkStr
-        .split('\n')
-        .filter((line) => line.startsWith('+'))
-        .map((line) => line.slice(1));
+        const addedRows = diffHunkStr
+          .split('\n')
+          .filter((line) => line.startsWith('+'))
+          .map((line) => line.slice(1));
+        if (isEmptyArray(addedRows)) return;
 
-      return {
-        lines: addedRows,
-        lineNumber: {
-          start: parseInt(startLineNumber),
-          count: parseInt(count),
-        },
-      };
-    });
+        return {
+          lines: addedRows,
+          lineNumber: {
+            start: parseInt(startLineNumber),
+            count: count === undefined ? 1 : parseInt(count),
+          },
+        };
+      })
+      .filter(
+        (hunk): hunk is Exclude<typeof hunk, undefined> => hunk !== undefined,
+      );
   }
 }
---
+++ src/utils/array.ts
@@ -0,0 +1,3 @@
+export function isEmptyArray(array: any[]): boolean {
+  return array.length === 0;
+}
