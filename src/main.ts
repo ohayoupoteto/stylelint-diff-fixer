b349ebe6c315df707bf03da808f4b9bf41b3ea29
oteto <94293017+ohayoupoteto@users.noreply.github.com>
2023-11-23 16:36:02 +0900

    refactor

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,14 @@ import { CssFileSystem } from './css-file/css-file-system';
 import { Logger } from './log/logger';
 import { LogStyle } from './log/log-style';
 import { CssFileHandler } from './css-file/css-file-handler';
+import { isEmptyArray } from './utils/array';
 
 export async function main(filepath: string): Promise<void> {
   const commandExecutor = new CommandExecutor();
   const diffParser = new DiffParser(commandExecutor);
 
   const diffsEachFile = diffParser.getDiffsEachFile(filepath);
-  if (diffsEachFile.length === 0) return;
+  if (isEmptyArray(diffsEachFile)) return;
 
   const styleFixer = new StyleFixer(new StyleAdjuster());
   const cssFileSystem = new CssFileSystem();
